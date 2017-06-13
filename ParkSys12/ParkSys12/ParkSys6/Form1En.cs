6d68e90889facf484522c920b026a1d9445db331
swordlink <swordlink@msn.com>
2017-06-14 00:28:48 +0800

    Add files via upload

--- ParkSys12/ParkSys12/ParkSys6/Form1En.cs
+++ ParkSys12/ParkSys12/ParkSys6/Form1En.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO.Ports;
+//using System.Windows.Controls.listboxitem;
 
 namespace WindowsFormsApplication1
 {
@@ -16,26 +17,37 @@ namespace WindowsFormsApplication1
         public string noPlate;
         public string lot;
         public string level;
-        public bool vacancy;  // parkig kosng ke tak
+        public bool vacancy;  // parking kosng ke tak
+        public string code;
+
     }
     
  
     public partial class Form1En : Form
     {
-       Park[] myPark = new Park[10];
+        Park[] myPark = new Park[10];
+        
         public string[] normalLot = new string[5] {"C1","C2","C3","C4","C5"};
 
         private SerialPort myData;
         public static int i = 0;
         public static string[] Car = new string[10];
+        List<string> items = new List<string>();
         public string plate;
+        private string choosen;
+        private int size;
         public Form1En()
         {
-            loadArray();
-            loadParkToList();
+           
             InitializeComponent();
-           // Init();
-            
+            loadArray();
+           // loadParkToList();
+           // loadCode();
+           // listBox1.DataSource = items;
+            listBox1.MouseDoubleClick += new MouseEventHandler(listBox1_DoubleClick);
+            // Init();
+           
+
         }
 
         private void button1_Click(object sender, EventArgs e)  //niii button save
@@ -43,15 +55,20 @@ namespace WindowsFormsApplication1
             label1.Text = "Gate Open";
             pictureBox1.Image=WindowsFormsApplication1.Properties.Resources.Open;
             //myData.Write("N");
+            //listBox1.DataSource = null;
+            //listBox1.DataSource = items;
             plate = textBox3.Text;
             
+            loadParkToList();
+          //  loadCode();
 
-           
-           // CarPark.SavePlate((textBox3.Text));
-           //string txt = CarPark.Car[i];
-           // MessageBox.Show(Car[i], "System");
+
+
+            // CarPark.SavePlate((textBox3.Text));
+            //string txt = CarPark.Car[i];
+            // MessageBox.Show(Car[i], "System");
             //i++;
-           
+
 
 
         }
@@ -60,10 +77,10 @@ namespace WindowsFormsApplication1
         {
 
         }
-
+        string[] b = new string[2];
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
+          
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -142,6 +159,7 @@ namespace WindowsFormsApplication1
                 myPark[i].index = i;
                 myPark[i].lot = normalLot[i];
                 myPark[i].level = "G";
+                myPark[i].vacancy = false;
                 j++;
 
             }
@@ -151,6 +169,7 @@ namespace WindowsFormsApplication1
                 myPark[j].index = j + 1;
                 myPark[j].lot = Reserve.lot[i];
                 myPark[j].level = "G";
+                myPark[j].vacancy = false;
                 j++;
             }
 
@@ -159,22 +178,113 @@ namespace WindowsFormsApplication1
                 myPark[j].index = j + 1;
                 myPark[j].lot = Disabled.lot[i];
                 myPark[j].level = "G";
+                myPark[j].vacancy = false;
                 j++;
 
             }
         }
 
-        private void loadParkToList ()
+        private  void loadParkToList ()
         {
+            //listBox1.ClearSelected();
+            listBox1.Items.Clear();
+            size = 0;
             for (int i = 0; i < 10; i++)
             {
-                Car[i] = myPark[i].lot;
-               
+                // Car[i] = myPark[i].lot;
+                //  items.Add(myPark[i].lot);
+                // listBox1.Items.Add(Car[i]);
+                if (myPark[i].vacancy==false)
+                {
+                    listBox1.Items.Add(myPark[i].lot);
+                    Car[size] = myPark[i].lot;
+                    size++;
+                }
+                else 
+                {
+                   // MessageBox.Show(myPark[i].lot);
+                }
+
             }
-        
-        
-        
-        
+             
+
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+       /* void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = this.listBox1.IndexFromPoint(e.Location);
+            if (index != System.Windows.Forms.ListBox.NoMatches)
+            {
+                MessageBox.Show(listBox1.SelectedItem.ToString());
+            }
+        }
+        */
+
+        private void listBox1_DoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = this.listBox1.IndexFromPoint(e.Location);
+            if (index != System.Windows.Forms.ListBox.NoMatches)
+            {
+                // MessageBox.Show(listBox1.SelectedItem.ToString());
+                string selected = listBox1.GetItemText(listBox1.SelectedItem);
+                // MessageBox.Show(myPark[3].code);
+               // string cuba = "C1";
+               
+                // int h =0;
+                string[] cod = new string[10];
+                cod[1] = selected;
+
+                int j = 0;
+               for (int i = 0;i<Car.Length;i++)
+                {
+                    
+                    if (myPark[i].lot == cod[1])
+                    {
+                         myPark[i].vacancy = true;
+                         myPark[i].noPlate = plate;
+                        
+                        MessageBox.Show("Congratulation !!");
+                        j = i;
+
+                    }
+                }
+                
+
+                 label4.Text ="PLATE: " + plate;
+                 label5.Text ="LOT: "+ myPark[j].lot;
+                Refresh();
+
+
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+        private void loadCode()
+        {
+               for (int i = 0;i<Car.Length;i++)
+               {
+                string selected = listBox1.GetItemText(listBox1.Items[i]);
+                myPark[i].code = selected;
+               }
+
+
+        }
+        */
     }
 }
--- ParkSys12/ParkSys12/ParkSys6/Form1En.designer.cs
+++ ParkSys12/ParkSys12/ParkSys6/Form1En.designer.cs
@@ -34,6 +34,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -96,11 +100,53 @@
             this.textBox3.TabIndex = 7;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(272, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "List of Avaible Car Park";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "PLATE:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "LOT:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
             // Form1En
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(333, 435);
+            this.ClientSize = new System.Drawing.Size(458, 435);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
@@ -124,6 +170,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
